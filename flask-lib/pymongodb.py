ada4c49781e3a83c5926421fca5a9bf3895e0454
Kongnanfei <kongnanfei@hmdata.com.cn>
2018-12-27 10:48:31 +0800

    mongoo

--- flask-lib/pymongodb.py
+++ flask-lib/pymongodb.py
@@ -68,3 +68,37 @@ mycol.drop()  # 删除集合
 # !!! 排序 !!!
 mydoc = mycol.find().sort("alexa")  # 升
 mydoc = mycol.find().sort("alexa", -1)  # 降
+
+'''
+嵌入式关系
+{
+   "_id":ObjectId("52ffc33cd85242f436000001"),
+   "contact": "987654321",
+   "dob": "01-01-1991",
+   "name": "Tom Benzamin",
+   "address": [
+      {
+         "building": "22 A, Indiana Apt",
+         "pincode": 123456,
+         "city": "Los Angeles",
+         "state": "California"
+      },
+      {
+         "building": "170 A, Acropolis Apt",
+         "pincode": 456789,
+         "city": "Chicago",
+         "state": "Illinois"
+      }]
+} 
+引用式关系
+{
+   "_id":ObjectId("52ffc33cd85242f436000001"),
+   "contact": "987654321",
+   "dob": "01-01-1991",
+   "name": "Tom Benzamin",
+   "address_ids": [
+      ObjectId("52ffc4a5d85242602e000000"),
+      ObjectId("52ffc4a5d85242602e000001")
+   ]
+}
+'''
